517956d7c2b3851333d6a126366301adb46dd95c
Avazbek <avazbeksharobov2@gmail.com>
2026-02-07 12:48:05 +0500

    Restore dashboard route

--- web/dashboard.js
+++ web/dashboard.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = (app) => {
   app.get('/dashboard', async (_, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dashboard.html'));
   });
 };
